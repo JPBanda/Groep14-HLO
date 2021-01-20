345c023ad53bca4f1ec57a505d792eb3e4ec6bb1
B <basbarg@gmail.com>
2021-01-20 16:20:04 +0100

    Externe API Fetching + Beginsel Collectables
    Hierbij wordt er een basis fetch van de API uitgevoerd en daarnaast bevat deze versie ook het beginsel van de collectables, nog niet duidelijk of deze getoond gaan worden bovenaan de camera of een alternatieve plek.
    
    Modellen zijn verplaatst naar de "modellen" map

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ window.onload = () => {
     const doors = document.getElementsByClassName("js--door");
     const buttons = document.getElementsByClassName("js--button");
     const hint_lamp = document.getElementsByClassName("js--gloeilamp")[0];
+    const block = document.getElementsByClassName("js--block")[0];
     let ani = document.createAttribute('animation');
     let bani = document.createAttribute('animation');
+    const text = document.getElementsByClassName("js--hint-text")[0];
+    const backdrop = document.getElementsByClassName("js--backdrop")[0];
+    const plaatje = document.getElementsByClassName("js--extern")[0];
+    const text_extern = document.getElementsByClassName("js--extern-text")[0];
 
-
+    let synth = window.speechSynthesis;
+    let utter = new SpeechSynthesisUtterance();
+    utter.lang = 'nl-NL';
+    utter.volume = 0.5;
 
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(evt){
@@ -21,10 +29,34 @@ window.onload = () => {
 
     hint_lamp.addEventListener("click", () => {
         hint_lamp.emit("test");
+        backdrop.setAttribute("position", "-4 1.5 0");
+        text.setAttribute("position", "-4 1.5 0");
     });
+
+
+    block.addEventListener("click", () => {
+        utter.text = text.getAttribute('value');
+        synth.speak(utter);
+    });
+    
+
+    fetch('https://api.gbif.org/v1/species/7380886')
+                .then(response => response.json())
+                .then(data => console.log(data));
+
+    fetch('https://api.gbif.org/v1/species/7380886')
+                .then(response => response.json())
+                .then(data => text_extern.setAttribute('value', "Naam: " + data.canonicalName + "\n" + "Soort: " + data.kingdom));
+
+    // Google search op images moet nog wel verbeterd worden aangezien hij moeilijke querries niet pakt
+    // fetch("https://www.googleapis.com/customsearch/v1?key=AIzaSyCEZPBtiVPhBdLpa0aDoJ1p1Bz3rwxf3pc&cx=ba3a73fb539676d10&q=Netherlands - Country+image")
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
 
     
 }
+
+
 
 
 
