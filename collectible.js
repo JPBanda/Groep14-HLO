fdaef6b598bf643f87b9cf9bcefabae0c598e6d3
MaxVssr <max.visser576@gmail.com>
2021-01-22 17:47:03 +0100

    Implemented walking, pickup, putdown

--- collectible.js
+++ collectible.js
@@ -1,33 +1,33 @@
-AFRAME.registerComponent('collectible', {
-    schema: {
+// AFRAME.registerComponent('collectible', {
+//     schema: {
         
-    },
+//     },
 
-    init: function () {
-      const collectibles = document.getElementsByClassName("js--item");
-      const camera = document.getElementById("js--camera");
+//     init: function () {
+//       const collectibles = document.getElementsByClassName("js--item");
+//       const camera = document.getElementById("js--camera");
 
-      this.collect = () =>{
-          for(let i = 0; i < collectibles.length; i++){
-            collectibles[i].addEventListener('click', function(evt) {
-                    camera.innerHTML += '<a-obj-model class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.1 0.55 -0.8" scale="0.01 0.01 0.01" rotation="0 0 0" animation="property: rotation; to: 0 360 0; loop: true; dur: 10000;" easing="linear"></a-obj-model>'
-                    this.remove();
-            });
-          }
-      }
+//       this.collect = () =>{
+//           for(let i = 0; i < collectibles.length; i++){
+//             collectibles[i].addEventListener('click', function(evt) {
+//                     camera.innerHTML += '<a-obj-model class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.1 0.55 -0.8" scale="0.01 0.01 0.01" rotation="0 0 0" animation="property: rotation; to: 0 360 0; loop: true; dur: 10000;" easing="linear"></a-obj-model>'
+//                     this.remove();
+//             });
+//           }
+//       }
 
-      this.el.addEventListener("click", this.collect);
-    },
+//       this.el.addEventListener("click", this.collect);
+//     },
 
-    update: function () {
-        this.collect();
-    },
+//     update: function () {
+//         this.collect();
+//     },
 
-    remove: function () {
-      // Do something the component or its entity is detached.
-    },
+//     remove: function () {
+//       // Do something the component or its entity is detached.
+//     },
 
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
-    }
-});
+//     tick: function (time, timeDelta) {
+//       // Do something on every scene tick or frame.
+//     }
+// });
--- pickup.js
+++ pickup.js
@@ -7,19 +7,20 @@ AFRAME.registerComponent('pickup', {
         const pickup = document.getElementsByClassName("js--pickup");
         const camera = document.getElementById("js--camera");
         const placeholder = document.getElementsByClassName("js--placeholder");
-        let scene = document.getElementById('js--scene')
+        let scene = document.getElementById('js--scene');
         let hold = null;
+        
+        
 
-        this.pickup = function () {
-
-            for (let i = 0; i < pickup.length; i++) {
-                pickup[i].addEventListener('click', function (evt) {
-                    if (hold == null) {
-                        camera.innerHTML += '<a-obj-model collectible class="js--hold js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>'
-                        hold = "item";
-                        this.remove();
-                    }
-                });
+        this.pickup = function() {
+            for (let i = 0; i<pickup.length; i++) {
+              pickup[i].addEventListener('click', function(evt){
+                if (hold==null) {
+                camera.innerHTML += '<a-obj-model collectible id="js--hold" class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>';
+                hold = "item";
+                this.remove();
+                }
+              });
             }
         }
 
@@ -28,28 +29,44 @@ AFRAME.registerComponent('pickup', {
                 placeholder[i].addEventListener('click', function(evt){
                     if (hold == "item") {
                         let box = document.createElement('a-obj-model');
-                        box.setAttribute("class", "js--hold js--item js--col1 js--interact");
+                        box.setAttribute("class", "js--item js--col1 js--interact");
                         box.setAttribute("src", "#microscope-obj")
                         box.setAttribute("mtl", "#microscope-mtl")
                         box.setAttribute("scale", "0.09 0.09 0.09")
                         box.setAttribute("position", {x: this.getAttribute('position').x, y:"0", z: 
                         this.getAttribute('position').z});
                         scene.appendChild(box);
-                        document.getElementById('js--hold').remove();
-                        addListeners();
+
                         hold = null;
+                    }
+                    else {
+                        this.pickup;
                     }
                 });
             }
         }
 
+        // this.pickupAgain = function() {
+        //     for (let i = 0; i < pickup.length; i++) {
+        //         pickup[i].addEventListener('click', function (evt) {
+        //             if (placed == "itemPlaced") {
+        //                 camera.innerHTML += '<a-obj-model collectible id="js--hold" class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>'
+        //                 hold = "item";
+        //                 this.remove();
+        //             }
+        //         });
+        //     }
+        // }
+
         this.el.addEventListener("click", this.pickup);
         this.el.addEventListener("click", this.putDown);
+        // this.el.addEventListener("click", this.pickupAgain);
     },
 
     update: function () {
         this.pickup();
         this.putDown();
+        // this.pickupAgain();
     },
 
     remove: function () {
