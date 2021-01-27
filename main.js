47f6ac94abd38f1bcf84313046c00ad35946ab50
MaxVssr <max.visser576@gmail.com>
2021-01-27 22:37:41 +0100

    Toegevoegd oppakken en neerzetten voor de 3 objecten
    Toegevoegd oppakken en neerzetten voor de 3 objecten en een component van gemaakt.
    Map 'modellen' verwijderd (was overbodig).
    Zelf model van preparaat gemaakt en toegevoegd aan de omgeving.
    Sink.gtlf van Bas toegevoegd aan de omgeving (nu wel de sink cut-out).
    Deur aan de linkerkant van de omgeving toegevoegd, deze moet open gaan als de 3 objecten op de goede plek staan (moet nog aan gewerkt worden).
    
    Bij de vorige push pythagoras toegevoegd voor het bewegen door de omgeving (vergeten bij de comment te zetten :)).

--- main.js
+++ main.js
@@ -1,101 +1,68 @@
 window.onload = () => {
-    const cursor = document.getElementById("js--cursor");
-    const doors = document.getElementsByClassName("js--door");
-    const buttons = document.getElementsByClassName("js--button");
-    const hint_lamp = document.getElementsByClassName("js--gloeilamp")[0];
-    const block = document.getElementsByClassName("js--block")[0];
-    let ani = document.createAttribute('animation');
-    let bani = document.createAttribute('animation');
-    const text = document.getElementsByClassName("js--hint-text")[0];
-    const backdrop = document.getElementsByClassName("js--backdrop")[0];
-    const plaatje = document.getElementsByClassName("js--extern")[0];
-    const text_extern = document.getElementsByClassName("js--extern-text")[0];
+  const cursor = document.getElementById("js--cursor");
+  const doors = document.getElementsByClassName("js--door");
+  const buttons = document.getElementsByClassName("js--button");
+  const hint_lamp = document.getElementsByClassName("js--gloeilamp")[0];
+  const block = document.getElementsByClassName("js--block")[0];
+  let ani = document.createAttribute("animation");
+  let bani = document.createAttribute("animation");
+  const text = document.getElementsByClassName("js--hint-text")[0];
+  const backdrop = document.getElementsByClassName("js--backdrop")[0];
+  const plaatje = document.getElementsByClassName("js--extern")[0];
+  const text_extern = document.getElementsByClassName("js--extern-text")[0];
 
-    const camera = document.getElementById('js--camera');
-    let scene = document.getElementById('js--scene');
-    let pickups = document.getElementsByClassName('js--pickup');
-    const placeholders = document.getElementsByClassName('js--placeholder');
-    let hold = null;
+  const camera = document.getElementById("js--camera");
+  let scene = document.getElementById("js--scene");
+  const microscope = document.getElementById("microscope");
+  const gasbrander = document.getElementById("gasbrander");
+  const preparaat = document.getElementById("preparaat");
+  const microscope_place = document.getElementById("microscope_place");
+  const gasbrander_place = document.getElementById("gasbrander_place");
+  const prep_place = document.getElementById("preparaat_place")
+  let holdms = null;
+  let holdgb = null;
+  let holdprep = null;
 
-    let synth = window.speechSynthesis;
-    let utter = new SpeechSynthesisUtterance();
-    utter.lang = 'nl-NL';
-    utter.volume = 0.5;
+  let synth = window.speechSynthesis;
+  let utter = new SpeechSynthesisUtterance();
+  utter.lang = "nl-NL";
+  utter.volume = 0.5;
 
-    for (let i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener('click', function(evt){
-            ani.value = 'property: position; easing: linear; dur: 5000; to:' + doors[i].getAttribute('position').x + " -1.6 " + doors[i].getAttribute('position').z;
-            doors[i].setAttribute('animation', ani.value);
-    
-            bani.value = 'property: rotation; easing: linear; dur: 1000; to: 135 0 0';
-            buttons[i].setAttribute('animation', bani.value);
-        });
-    } 
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (evt) {
+      ani.value =
+        "property: position; easing: linear; dur: 5000; to:" +
+        doors[i].getAttribute("position").x +
+        " -1.6 " +
+        doors[i].getAttribute("position").z;
+      doors[i].setAttribute("animation", ani.value);
 
-
-    hint_lamp.addEventListener("click", () => {
-        hint_lamp.emit("test");
-        backdrop.setAttribute("position", "-4 1.5 0");
-        text.setAttribute("position", "-4 1.5 0");
+      bani.value = "property: rotation; easing: linear; dur: 1000; to: 135 0 0";
+      buttons[i].setAttribute("animation", bani.value);
     });
+  }
 
+  hint_lamp.addEventListener("click", () => {
+    hint_lamp.emit("test");
+    backdrop.setAttribute("position", "-4 1.5 0");
+    text.setAttribute("position", "-4 1.5 0");
+  });
 
-    // block.addEventListener("click", () => {
-    //     utter.text = text.getAttribute('value');
-    //     synth.speak(utter);
-    // });
-    
+  // block.addEventListener("click", () => {
+  //     utter.text = text.getAttribute('value');
+  //     synth.speak(utter);
+  // });
 
-    // fetch('https://api.gbif.org/v1/species/7380886')
-    //             .then(response => response.json())
-    //             .then(data => console.log(data));
+  // fetch('https://api.gbif.org/v1/species/7380886')
+  //             .then(response => response.json())
+  //             .then(data => console.log(data));
 
-    // fetch('https://api.gbif.org/v1/species/7380886')
-    //             .then(response => response.json())
-    //             .then(data => text_extern.setAttribute('value', "Naam: " + data.canonicalName + "\n" + "Soort: " + data.kingdom));
+  // fetch('https://api.gbif.org/v1/species/7380886')
+  //             .then(response => response.json())
+  //             .then(data => text_extern.setAttribute('value', "Naam: " + data.canonicalName + "\n" + "Soort: " + data.kingdom));
 
-    // Google search op images moet nog wel verbeterd worden aangezien hij moeilijke querries niet pakt
-    // fetch("https://www.googleapis.com/customsearch/v1?key=AIzaSyCEZPBtiVPhBdLpa0aDoJ1p1Bz3rwxf3pc&cx=ba3a73fb539676d10&q=Netherlands - Country+image")
-    //     .then(response => response.json())
-    //     .then(data => console.log(data));
-
-    
-    function pickup() {
-        for (let i = 0; i < pickups.length; i++) {
-          pickups[i].addEventListener('click', function (evt) {
-            if (hold == null) {
-              camera.innerHTML += '<a-obj-model collectible id="js--hold" class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>';
-              hold = "box";
-              this.remove();
-            }
-          });
-        }
-      }
-    
-      pickup();
-    
-      for (let i = 0; i < placeholders.length; i++) {
-        placeholders[i].addEventListener('click', function (evt) {
-          if (hold == "box") {
-            let box = document.createElement('a-obj-model');
-            box.setAttribute("class", "js--pickup js--item js--col1 js--interact");
-            box.setAttribute("src", "#microscope-obj")
-            box.setAttribute("mtl", "#microscope-mtl")
-            box.setAttribute("scale", "0.09 0.09 0.09")
-            box.setAttribute("position", {x: this.getAttribute('position').x, y:"0", z: 
-            this.getAttribute('position').z});
-            scene.appendChild(box);
-            document.getElementById('js--hold').remove();
-            pickup();
-            hold = null;
-          }
-        });
-      }
-}
-
-
-
-
-
-
-
+  // Google search op images moet nog wel verbeterd worden aangezien hij moeilijke querries niet pakt
+  // fetch("https://www.googleapis.com/customsearch/v1?key=AIzaSyCEZPBtiVPhBdLpa0aDoJ1p1Bz3rwxf3pc&cx=ba3a73fb539676d10&q=Netherlands - Country+image")
+  //     .then(response => response.json())
+  //     .then(data => console.log(data));
+};
--- pickup.js
+++ pickup.js
@@ -1,74 +1,150 @@
-// AFRAME.registerComponent('pickup', {
-//     schema: {
+AFRAME.registerComponent("pickup", {
+  schema: {},
 
-//     },
+  init: function () {
+    const camera = document.getElementById("js--camera");
+    let scene = document.getElementById("js--scene");
+    const microscope = document.getElementById("microscope");
+    const gasbrander = document.getElementById("gasbrander");
+    const preparaat = document.getElementById("preparaat");
+    const microscope_place = document.getElementById("microscope_place");
+    const gasbrander_place = document.getElementById("gasbrander_place");
+    const prep_place = document.getElementById("preparaat_place");
+    let holdms = null;
+    let holdgb = null;
+    let holdprep = null;
+    const door = document.getElementsByClassName("js--door");
+    let ani = document.createAttribute("animation");
+    let holdms_placed = false;
+    let holdgb_placed = false;
+    let holdprep_placed = false;
 
-//     init: function () {
-//         const pickup = document.getElementsByClassName("js--pickup");
-//         const camera = document.getElementById("js--camera");
-//         const placeholder = document.getElementsByClassName("js--placeholder");
-//         let scene = document.getElementById('js--scene')
-//         let hold = null;
-//         let placed = null;
+    this.pickup_ms = function () {
+      microscope.addEventListener("click", function (evt) {
+        if (holdms == null && document.getElementById("microscope")) {
+          camera.innerHTML +=
+            '<a-obj-model collectible id="js--hold" class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>';
+          holdms = "item";
+          document.getElementById("microscope").remove();
+        }
+      });
+    };
 
-//         this.pickup = function () {
-//             for (let i = 0; i < pickup.length; i++) {
-//                 pickup[i].addEventListener('mouseenter', function (evt) {
-//                     let box;
-//                     if (hold == null && placed == null) {
-//                         camera.innerHTML += '<a-obj-model collectible id="js--hold" class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>'
-//                         hold = "item";
-//                         this.remove();
-//                     }
+    this.pickup_gb = function () {
+      gasbrander.addEventListener("click", function (evt) {
+        if (holdgb == null && document.getElementById("gasbrander")) {
+          camera.innerHTML +=
+            '<a-gltf-model id="js--gasbrander" src="#gasbrander-gltf" position="1.2 -1 -1" scale="1.2 1.2 1.2"></a-gltf-model>';
+          holdgb = "hold";
+          document.getElementById("gasbrander").remove();
+        }
+      });
+    };
 
-//                     if (placed == "itemPlaced" && hold == null) {
-//                         camera.innerHTML += '<a-obj-model collectible id="js--hold" class="js--item js--col1 js--interact" src="#microscope-obj" mtl="#microscope-mtl" position="1.2 -1 -1" scale="0.09 0.09 0.09"></a-obj-model>'
-//                         hold = "placed";
-//                         placed = null;
-                        
-//                     }
-//                 });
-//             }
-//         }
+    this.pickup_prep = function () {
+      preparaat.addEventListener("click", function (evt) {
+        if (holdprep == null && document.getElementById("preparaat")) {
+          camera.innerHTML +=
+            '<a-gltf-model id="js--prep" class="js--interact" src="#preparaat-gltf" position="0.8 -0.5 -0.6" scale="0.04 0.04 0.04"></a-gltf-model>';
+          holdprep = "prep";
+          document.getElementById("preparaat").remove();
+        }
+      });
+    };
 
-//         this.putDown = function () {
-//             for (let i=0; i<placeholder.length; i++) {
-//                 placeholder[i].addEventListener('mouseenter', function(evt){
-//                     if (hold == "item" && placed == null) {
-//                         let box = document.createElement('a-obj-model');
-//                         // box.setAttribute("id", "js--placed")
-//                         box.setAttribute("class", "js--pickup js--item js--col1 js--interact");
-//                         box.setAttribute("src", "#microscope-obj")
-//                         box.setAttribute("mtl", "#microscope-mtl")
-//                         box.setAttribute("scale", "0.09 0.09 0.09")
-//                         box.setAttribute("position", {x: this.getAttribute('position').x, y:"0", z: 
-//                         this.getAttribute('position').z});
-//                         scene.appendChild(box);
-//                         hold = null;
-//                         placed = "itemPlaced"
-//                         document.getElementById('js--hold').remove();
-//                     } 
-//                 });
-//             }
-//         }
+    this.putdown_ms = function () {
+      microscope_place.addEventListener("click", function (evt) {
+        if (holdms == "item" && document.getElementById("js--hold")) {
+          let microscope = document.createElement("a-obj-model");
+          microscope.setAttribute(
+            "class",
+            "js--pickup js--item js--col1 js--interact"
+          );
+          microscope.setAttribute("src", "#microscope-obj");
+          microscope.setAttribute("mtl", "#microscope-mtl");
+          microscope.setAttribute("scale", "0.09 0.09 0.09");
+          microscope.setAttribute("position", {
+            x: this.getAttribute("position").x,
+            y: "1.1",
+            z: this.getAttribute("position").z,
+          });
+          scene.appendChild(microscope);
+          document.getElementById("js--hold").remove();
+          holdms_placed == true;
+        }
+      });
+    };
 
-        
-//         this.el.addEventListener("mouseenter", this.pickup);
-//         this.el.addEventListener("mouseenter", this.putDown);
-        
-        
-//     },
+    this.putdown_gb = function () {
+      gasbrander_place.addEventListener("click", function (evt) {
+        if (holdgb == "hold" && document.getElementById("js--gasbrander")) {
+          let gasbrander = document.createElement("a-gltf-model");
+          gasbrander.setAttribute("class", "js--pickup js--interact");
+          gasbrander.setAttribute("src", "#gasbrander-gltf");
+          gasbrander.setAttribute("scale", "1.2 1.2 1.2");
+          gasbrander.setAttribute("position", {
+            x: this.getAttribute("position").x,
+            y: "0.9",
+            z: this.getAttribute("position").z,
+          });
+          scene.appendChild(gasbrander);
+          document.getElementById("js--gasbrander").remove();
+          holdgb_placed == true;
+        }
+      });
+    };
 
-//     update: function () {
-//         this.pickup();
-//         this.putDown();
-//     },
+    this.putdown_prep = function () {
+      prep_place.addEventListener("click", function (evt) {
+        if (holdprep == "prep" && document.getElementById("js--prep")) {
+          let prep = document.createElement("a-gltf-model");
+          prep.setAttribute("class", "js--pickup js--interact");
+          prep.setAttribute("src", "#preparaat-gltf");
+          prep.setAttribute("scale", "0.04 0.04 0.04");
+          prep.setAttribute("position", {
+            x: this.getAttribute("position").x,
+            y: "1.05",
+            z: this.getAttribute("position").z,
+          });
+          scene.appendChild(prep);
+          document.getElementById("js--prep").remove();
+          holdprep_placed == true;
+        }
+      });
+    };
 
-//     remove: function () {
-//         // Do something the component or its entity is detached.
-//     },
+    this.doorUnlock = function () {
+        if (holdms_placed == true && holdgb_placed == true && holdprep_placed == true) {
+            console.log("goed");
+          ani.value = 'property: position; easing: linear; dur: 5000; to:' + door.getAttribute('position').x + " -4.6 " + door.getAttribute('position').z;
+          door.setAttribute('animation', ani.value);
+        }
+      };
 
-//     tick: function (time, timeDelta) {
-//         // Do something on every scene tick or frame.
-//     }
-// });
+
+    this.el.addEventListener("click", this.pickup_ms);
+    this.el.addEventListener("click", this.putdown_ms);
+    this.el.addEventListener("click", this.pickup_gb);
+    this.el.addEventListener("click", this.putdown_gb);
+    this.el.addEventListener("click", this.pickup_prep);
+    this.el.addEventListener("click", this.putdown_prep);
+  },
+
+  update: function () {
+    this.pickup_ms();
+    this.putdown_ms();
+    this.pickup_gb();
+    this.putdown_gb();
+    this.pickup_prep();
+    this.putdown_prep();
+    this.doorUnlock();
+  },
+
+  remove: function () {
+    // Do something the component or its entity is detached.
+  },
+
+  tick: function (time, timeDelta) {
+    // Do something on every scene tick or frame.
+  },
+});
