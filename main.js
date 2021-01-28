cec17bad4e4d5454158475a0cab7ec33b7fc1a40
Baeshee <basbarg@gmail.com>
2021-01-28 19:45:16 +0100

    Eerste opzet 3e kamer
    Basis staat interactie nog geen JS interactie

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 window.onload = () => {
-    const secret_button = document.getElementsByClassName("js--secret");
-    const secret_door = document.getElementsByClassName("js--secret_door");
-    let secret_ani = document.createAttribute('animation');
-    let secret_bani = document.createAttribute('animation');
-
-
-        for (let i = 0; i < secret_button.length; i++){
-            secret_button[i].addEventListener('click', function(evt){
-                secret_bani.value = 'property: rotation; easing: linear; dur: 1000; to: -135 0 0';
-                secret_button[i].setAttribute('animation', secret_bani.value);
     
-                // Foute antwoorden
-                if(i == 0){
-                    secret_ani.value = 'property: position; easing: linear; dur: 5000; to:' + secret_door[0].getAttribute('position').x + " -4.6 " + secret_door[0].getAttribute('position').z;
-                    secret_door[0].setAttribute('animation', ani.value);
-                }
-            });
 
-        }
+
+        
 
 
     // let synth = window.speechSynthesis;
